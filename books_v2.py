5d29abc9ef43604a2d2efb6d5fd432e463a37996
Marcello Graco <marcello.graco@dixter.com.br>
2022-09-03 11:08:25 -0300

    FastAPI Project 2 terminado

--- books_v2.py
+++ books_v2.py
@@ -1,9 +1,100 @@
+from typing import Optional
+from venv import create
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
 
 app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(title="Description of the book",
+                                       max_length=100,
+                                       min_length=1)
+    rating: int = Field(gt=-1, lt=101)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "4470a3fa-e7e3-43fe-bcdf-e9d81eee2854",
+                "title": "Computer Science Pro",
+                "author": "Codingwithroby",
+                "description": "A very nice description of a book",
+                "rating": 75
+            }
+        }
 
 BOOKS = []
 
 @app.get("/")
-async def read_all_books():
+async def read_all_books(books_to_return: Optional[int] = None):
+    if len(BOOKS) < 1:
+        create_books_no_api()
+
+    if books_to_return and len(BOOKS) >= books_to_return > 0:
+        i = 1
+        new_books = []
+        while i <= books_to_return:
+            new_books.append(BOOKS[i - 1])
+            i += 1
+        return new_books
     return BOOKS
+
+@app.get("/book/{book_id}")
+async def read_book(book_id:UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+
+@app.post("/")
+async def create_book(book: Book):
+    BOOKS.append(book)
+    return book
+
+@app.put("/{book_id}")
+async def update_book(book_id: UUID, book: Book):
+    counter = 0
+
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            BOOKS[counter - 1] = book
+            return BOOKS[counter - 1]
+
+@app.delete("/{book_id}")
+async def delete_book(book_id: UUID):
+    counter = 0
+
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            del BOOKS[counter - 1]
+            return f'ID:{book_id} deleted'
+
+def create_books_no_api():
+    book_1 = Book(id="4470a3fa-e7e3-43fe-bcdf-e9d81eee2854",
+                  title="Title 1",
+                  author="Author 1",
+                  description="Description 1",
+                  rating=60)
+    book_2 = Book(id="52d96ccc-0e94-41c2-bf62-b785c25c3734",
+                  title="Title 2",
+                  author="Author 2",
+                  description="Description 2",
+                  rating=60)
+    book_3 = Book(id="cf89fa32-a46f-4530-b555-a08e251dea60",
+                  title="Title 3",
+                  author="Author 3",
+                  description="Description 3",
+                  rating=60)
+    book_4 = Book(id="84c67957-6c48-4c09-8ba2-600781f0c30a",
+                  title="Title 4",
+                  author="Author 4",
+                  description="Description 4",
+                  rating=60)
+    BOOKS.append(book_1)
+    BOOKS.append(book_2)
+    BOOKS.append(book_3)
+    BOOKS.append(book_4)
